de156f698baefbd18cda6f6f534c8799794bfbc9
YinBoran <YinBoran@users.noreply.github.com>
2026-06-15 13:15:13 +0000

    deploy: 7bec8cce40dcafb01ff8b5a1fdbec5ee12767fa5

---
+++ assets/icons-7TJdTAP5.js
@@ -0,0 +1,159 @@
+function L(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}var R={exports:{}},n={};/**
+ * @license React
+ * react.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var d=Symbol.for("react.element"),I=Symbol.for("react.portal"),H=Symbol.for("react.fragment"),T=Symbol.for("react.strict_mode"),D=Symbol.for("react.profiler"),N=Symbol.for("react.provider"),F=Symbol.for("react.context"),U=Symbol.for("react.forward_ref"),Z=Symbol.for("react.suspense"),X=Symbol.for("react.memo"),G=Symbol.for("react.lazy"),x=Symbol.iterator;function W(e){return e===null||typeof e!="object"?null:(e=x&&e[x]||e["@@iterator"],typeof e=="function"?e:null)}var j={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}},E=Object.assign,$={};function h(e,t,r){this.props=e,this.context=t,this.refs=$,this.updater=r||j}h.prototype.isReactComponent={};h.prototype.setState=function(e,t){if(typeof e!="object"&&typeof e!="function"&&e!=null)throw Error("setState(...): takes an object of state variables to update or a function which returns an object of state variables.");this.updater.enqueueSetState(this,e,t,"setState")};h.prototype.forceUpdate=function(e){this.updater.enqueueForceUpdate(this,e,"forceUpdate")};function A(){}A.prototype=h.prototype;function g(e,t,r){this.props=e,this.context=t,this.refs=$,this.updater=r||j}var M=g.prototype=new A;M.constructor=g;E(M,h.prototype);M.isPureReactComponent=!0;var C=Array.isArray,P=Object.prototype.hasOwnProperty,w={current:null},V={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var o,a={},u=null,i=null;if(t!=null)for(o in t.ref!==void 0&&(i=t.ref),t.key!==void 0&&(u=""+t.key),t)P.call(t,o)&&!V.hasOwnProperty(o)&&(a[o]=t[o]);var l=arguments.length-2;if(l===1)a.children=r;else if(1<l){for(var s=Array(l),y=0;y<l;y++)s[y]=arguments[y+2];a.children=s}if(e&&e.defaultProps)for(o in l=e.defaultProps,l)a[o]===void 0&&(a[o]=l[o]);return{$$typeof:d,type:e,key:u,ref:i,props:a,_owner:w.current}}function K(e,t){return{$$typeof:d,type:e.type,key:t,ref:e.ref,props:e.props,_owner:e._owner}}function S(e){return typeof e=="object"&&e!==null&&e.$$typeof===d}function J(e){var t={"=":"=0",":":"=2"};return"$"+e.replace(/[=:]/g,function(r){return t[r]})}var b=/\/+/g;function _(e,t){return typeof e=="object"&&e!==null&&e.key!=null?J(""+e.key):t.toString(36)}function m(e,t,r,o,a){var u=typeof e;(u==="undefined"||u==="boolean")&&(e=null);var i=!1;if(e===null)i=!0;else switch(u){case"string":case"number":i=!0;break;case"object":switch(e.$$typeof){case d:case I:i=!0}}if(i)return i=e,a=a(i),e=o===""?"."+_(i,0):o,C(a)?(r="",e!=null&&(r=e.replace(b,"$&/")+"/"),m(a,t,r,"",function(y){return y})):a!=null&&(S(a)&&(a=K(a,r+(!a.key||i&&i.key===a.key?"":(""+a.key).replace(b,"$&/")+"/")+e)),t.push(a)),1;if(i=0,o=o===""?".":o+":",C(e))for(var l=0;l<e.length;l++){u=e[l];var s=o+_(u,l);i+=m(u,t,r,s,a)}else if(s=W(e),typeof s=="function")for(e=s.call(e),l=0;!(u=e.next()).done;)u=u.value,s=o+_(u,l++),i+=m(u,t,r,s,a);else if(u==="object")throw t=String(e),Error("Objects are not valid as a React child (found: "+(t==="[object Object]"?"object with keys {"+Object.keys(e).join(", ")+"}":t)+"). If you meant to render a collection of children, use an array instead.");return i}function k(e,t,r){if(e==null)return e;var o=[],a=0;return m(e,o,"","",function(u){return t.call(r,u,a++)}),o}function Q(e){if(e._status===-1){var t=e._result;t=t(),t.then(function(r){(e._status===0||e._status===-1)&&(e._status=1,e._result=r)},function(r){(e._status===0||e._status===-1)&&(e._status=2,e._result=r)}),e._status===-1&&(e._status=0,e._result=t)}if(e._status===1)return e._result.default;throw e._result}var f={current:null},v={transition:null},Y={ReactCurrentDispatcher:f,ReactCurrentBatchConfig:v,ReactCurrentOwner:w};function z(){throw Error("act(...) is not supported in production builds of React.")}n.Children={map:k,forEach:function(e,t,r){k(e,function(){t.apply(this,arguments)},r)},count:function(e){var t=0;return k(e,function(){t++}),t},toArray:function(e){return k(e,function(t){return t})||[]},only:function(e){if(!S(e))throw Error("React.Children.only expected to receive a single React element child.");return e}};n.Component=h;n.Fragment=H;n.Profiler=D;n.PureComponent=g;n.StrictMode=T;n.Suspense=Z;n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED=Y;n.act=z;n.cloneElement=function(e,t,r){if(e==null)throw Error("React.cloneElement(...): The argument must be a React element, but you passed "+e+".");var o=E({},e.props),a=e.key,u=e.ref,i=e._owner;if(t!=null){if(t.ref!==void 0&&(u=t.ref,i=w.current),t.key!==void 0&&(a=""+t.key),e.type&&e.type.defaultProps)var l=e.type.defaultProps;for(s in t)P.call(t,s)&&!V.hasOwnProperty(s)&&(o[s]=t[s]===void 0&&l!==void 0?l[s]:t[s])}var s=arguments.length-2;if(s===1)o.children=r;else if(1<s){l=Array(s);for(var y=0;y<s;y++)l[y]=arguments[y+2];o.children=l}return{$$typeof:d,type:e.type,key:a,ref:u,props:o,_owner:i}};n.createContext=function(e){return e={$$typeof:F,_currentValue:e,_currentValue2:e,_threadCount:0,Provider:null,Consumer:null,_defaultValue:null,_globalName:null},e.Provider={$$typeof:N,_context:e},e.Consumer=e};n.createElement=q;n.createFactory=function(e){var t=q.bind(null,e);return t.type=e,t};n.createRef=function(){return{current:null}};n.forwardRef=function(e){return{$$typeof:U,render:e}};n.isValidElement=S;n.lazy=function(e){return{$$typeof:G,_payload:{_status:-1,_result:e},_init:Q}};n.memo=function(e,t){return{$$typeof:X,type:e,compare:t===void 0?null:t}};n.startTransition=function(e){var t=v.transition;v.transition={};try{e()}finally{v.transition=t}};n.unstable_act=z;n.useCallback=function(e,t){return f.current.useCallback(e,t)};n.useContext=function(e){return f.current.useContext(e)};n.useDebugValue=function(){};n.useDeferredValue=function(e){return f.current.useDeferredValue(e)};n.useEffect=function(e,t){return f.current.useEffect(e,t)};n.useId=function(){return f.current.useId()};n.useImperativeHandle=function(e,t,r){return f.current.useImperativeHandle(e,t,r)};n.useInsertionEffect=function(e,t){return f.current.useInsertionEffect(e,t)};n.useLayoutEffect=function(e,t){return f.current.useLayoutEffect(e,t)};n.useMemo=function(e,t){return f.current.useMemo(e,t)};n.useReducer=function(e,t,r){return f.current.useReducer(e,t,r)};n.useRef=function(e){return f.current.useRef(e)};n.useState=function(e){return f.current.useState(e)};n.useSyncExternalStore=function(e,t,r){return f.current.useSyncExternalStore(e,t,r)};n.useTransition=function(){return f.current.useTransition()};n.version="18.3.1";R.exports=n;var p=R.exports;const ne=L(p);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ee=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),O=(...e)=>e.filter((t,r,o)=>!!t&&o.indexOf(t)===r).join(" ");/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var te={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const re=p.forwardRef(({color:e="currentColor",size:t=24,strokeWidth:r=2,absoluteStrokeWidth:o,className:a="",children:u,iconNode:i,...l},s)=>p.createElement("svg",{ref:s,...te,width:t,height:t,stroke:e,strokeWidth:o?Number(r)*24/Number(t):r,className:O("lucide",a),...l},[...i.map(([y,B])=>p.createElement(y,B)),...Array.isArray(u)?u:[u]]));/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=(e,t)=>{const r=p.forwardRef(({className:o,...a},u)=>p.createElement(re,{ref:u,iconNode:t,className:O(`lucide-${ee(e)}`,o),...a}));return r.displayName=`${e}`,r};/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const oe=c("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ae=c("BarChart3",[["path",{d:"M3 3v18h18",key:"1s2lah"}],["path",{d:"M18 17V9",key:"2bz60n"}],["path",{d:"M13 17V5",key:"1frdt8"}],["path",{d:"M8 17v-3",key:"17ska0"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ue=c("Bell",[["path",{d:"M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9",key:"1qo2s2"}],["path",{d:"M10.3 21a1.94 1.94 0 0 0 3.4 0",key:"qgo35s"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ce=c("Bookmark",[["path",{d:"m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z",key:"1fy3hk"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const se=c("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const le=c("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ie=c("Gift",[["rect",{x:"3",y:"8",width:"18",height:"4",rx:"1",key:"bkv52"}],["path",{d:"M12 8v13",key:"1c76mn"}],["path",{d:"M19 12v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7",key:"6wjy6b"}],["path",{d:"M7.5 8a2.5 2.5 0 0 1 0-5A4.8 8 0 0 1 12 8a4.8 8 0 0 1 4.5-5 2.5 2.5 0 0 1 0 5",key:"1ihvrl"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const fe=c("Heart",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ye=c("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const pe=c("MessageCircle",[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const he=c("Minus",[["path",{d:"M5 12h14",key:"1ays0h"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const de=c("Moon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ke=c("Package",[["path",{d:"m7.5 4.27 9 5.15",key:"1c824w"}],["path",{d:"M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z",key:"hh9hay"}],["path",{d:"m3.3 7 8.7 5 8.7-5",key:"g66t2b"}],["path",{d:"M12 22V12",key:"d0xqtd"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const me=c("Phone",[["path",{d:"M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z",key:"foiqr5"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ve=c("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _e=c("Rocket",[["path",{d:"M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z",key:"m3kijz"}],["path",{d:"m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z",key:"1fmvmk"}],["path",{d:"M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0",key:"1f8sc4"}],["path",{d:"M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5",key:"qeys4"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ge=c("Settings",[["path",{d:"M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z",key:"1qme2f"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Me=c("Share2",[["circle",{cx:"18",cy:"5",r:"3",key:"gq8acd"}],["circle",{cx:"6",cy:"12",r:"3",key:"w7nqdw"}],["circle",{cx:"18",cy:"19",r:"3",key:"1xt0gg"}],["line",{x1:"8.59",x2:"15.42",y1:"13.51",y2:"17.49",key:"47mynk"}],["line",{x1:"15.41",x2:"8.59",y1:"6.51",y2:"10.49",key:"1n3mei"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const we=c("ShoppingBag",[["path",{d:"M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z",key:"hou9p0"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M16 10a4 4 0 0 1-8 0",key:"1ltviw"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Se=c("ShoppingCart",[["circle",{cx:"8",cy:"21",r:"1",key:"jimo8o"}],["circle",{cx:"19",cy:"21",r:"1",key:"13723u"}],["path",{d:"M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12",key:"9zh506"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const xe=c("Star",[["polygon",{points:"12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2",key:"8f66p6"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Ce=c("Sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const be=c("Tag",[["path",{d:"M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z",key:"vktsd0"}],["circle",{cx:"7.5",cy:"7.5",r:".5",fill:"currentColor",key:"kqv944"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Re=c("Volume2",[["polygon",{points:"11 5 6 9 2 9 2 15 6 15 11 19 11 5",key:"16drj5"}],["path",{d:"M15.54 8.46a5 5 0 0 1 0 7.07",key:"ltjumu"}],["path",{d:"M19.07 4.93a10 10 0 0 1 0 14.14",key:"1kegas"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const je=c("VolumeX",[["polygon",{points:"11 5 6 9 2 9 2 15 6 15 11 19 11 5",key:"16drj5"}],["line",{x1:"22",x2:"16",y1:"9",y2:"15",key:"1ewh16"}],["line",{x1:"16",x2:"22",y1:"9",y2:"15",key:"5ykzw1"}]]);/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Ee=c("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{oe as A,ae as B,le as C,ie as G,fe as H,ye as M,ke as P,_e as R,xe as S,be as T,je as V,Ee as X,Se as a,Re as b,pe as c,Me as d,me as e,de as f,Ce as g,we as h,ue as i,ge as j,se as k,ve as l,ce as m,he as n,ne as o,p as r};
---
+++ sw.js
@@ -0,0 +1,102 @@
+const V            = "yb-v1";
+const CACHE_MEDIA  = V + "-media";
+const CACHE_API    = V + "-api";
+const CACHE_APP    = V + "-app";
+
+const APP_SHELL = ["/home/", "/placeholder.jpg"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_APP)
+      .then(function(c) { return c.addAll(APP_SHELL).catch(function(){}); })
+      .then(function()  { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) {
+          return ![CACHE_MEDIA, CACHE_API, CACHE_APP].includes(k);
+        }).map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() { return clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  var url = e.request.url;
+  if (url.includes("/media/") || url.includes("/cdn-cgi/image/")) {
+    e.respondWith(cacheFirst(e.request, CACHE_MEDIA)); return;
+  }
+  if (url.includes("/api/")) {
+    e.respondWith(networkFirst(e.request, CACHE_API)); return;
+  }
+  if (url.match(/\.(js|css|html|json|webp|png|jpg|svg)$/)) {
+    e.respondWith(cacheFirst(e.request, CACHE_APP)); return;
+  }
+});
+
+async function cacheFirst(req, name) {
+  var c = await caches.open(name);
+  var hit = await c.match(req);
+  if (hit) return hit;
+  try {
+    var res = await fetch(req);
+    if (res.ok) c.put(req, res.clone());
+    return res;
+  } catch (_) {
+    return (await caches.match("/placeholder.jpg")) || new Response("Offline", { status: 503 });
+  }
+}
+
+async function networkFirst(req, name) {
+  try {
+    var res = await fetch(req);
+    var c = await caches.open(name);
+    if (res.ok) c.put(req, res.clone());
+    return res;
+  } catch (_) {
+    var hit = await caches.match(req);
+    return hit || new Response(
+      JSON.stringify({ ok: false, data: [], source: "offline", msg: { text: "Offline", type: "error" } }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
+
+self.addEventListener("message", async function(e) {
+  var d = e.data;
+  if (!d || !d.type) return;
+  if (d.type === "PRECACHE_VIDEO") {
+    var c = await caches.open(CACHE_MEDIA);
+    if (!(await c.match(d.url))) {
+      try { var r = await fetch(d.url); if (r.ok) await c.put(d.url, r); } catch (_) {}
+    }
+  }
+  if (d.type === "PRECACHE_THUMBS") {
+    var c2 = await caches.open(CACHE_MEDIA);
+    for (var i = 0; i < (d.urls || []).length; i++) {
+      try {
+        if (!(await c2.match(d.urls[i]))) {
+          var r2 = await fetch(d.urls[i]); if (r2.ok) await c2.put(d.urls[i], r2);
+        }
+      } catch (_) {}
+    }
+  }
+  if (d.type === "RECONNECT_EVICT") {
+    var c3 = await caches.open(CACHE_MEDIA);
+    var keys = await c3.keys();
+    var vm = d.viewMap || {};
+    var sorted = keys.map(function(r) { return { r: r, v: vm[r.url] || 0 }; })
+      .sort(function(a, b) { return b.v - a.v; });
+    for (var j = Math.floor(sorted.length / 2); j < sorted.length; j++) {
+      await c3.delete(sorted[j].r);
+    }
+  }
+  if (d.type === "CLEAR_CACHE") {
+    var all = await caches.keys();
+    await Promise.all(all.map(function(k) { return caches.delete(k); }));
+  }
+});
